387b0388e00e91b8bdbc114f32ef1ba11a0e7dfa
Daniil210 <den.iv0008@gmail.com>
2021-07-23 12:43:47 +0300

    !

---
+++ 2.2.2. spring_mvc/src/main/java/web/dao/CarDaoImpl.java
@@ -0,0 +1,26 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+@Repository
+public class CarDaoImpl implements CarDao{
+
+    public static List<Car> carList = new ArrayList<>();
+
+    static {
+        carList.add(new Car("Mercedes-Benz M-Класс II (W164) 350",2007, 650000.00));
+        carList.add(new Car("Hyundai ix35 I Рестайлинг", 2015, 945000));
+        carList.add(new Car("Land Rover Range Rover Evoque I 6-speed", 2012, 1490000));
+        carList.add(new Car("Peugeot 408 I Рестайлинг",2017, 984000.00));
+        carList.add(new Car("Volkswagen Touareg II",2013, 1797000));
+    }
+
+    @Override
+    public List<Car> getCars(int count) {
+
+        return carList.subList(0, count);
+    }
+}
